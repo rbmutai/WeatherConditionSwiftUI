6c0aadbc5636fcfa9fee3714a3ec20c08c27d4c6
Robert Mutai <rbmutai@gmail.com>
2025-01-22 12:59:47 +0300

    add favourites view model

---
+++ WeatherConditionSwiftUI/FavouritesViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  FavouritesViewModel.swift
+//  WeatherConditionSwiftUI
+//
+//  Created by Robert Mutai on 22/01/2025.
+//
+
+import Foundation
+import Combine
+import MapKit
+class FavouritesViewModel {
+    
+    @Published var favouriteLocations: [FavouriteLocationDetail] = []
+    @Published var errorMessage = ""
+    @Published var mapAnnotations : [MapAnnotation] = []
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+    let persistence = PersistenceController.shared
+    
+    func loadFavouriteLocations() {
+        favouriteLocations = persistence.getFaouriteLocations()
+        mapAnnotations = []
+        
+        if favouriteLocations.count == 0 {
+            errorMessage = "No Locations Saved!"
+            
+        } else {
+            var annotations: [MapAnnotation] = []
+            for item in favouriteLocations {
+                let annotation = MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: item.latitude, longitude: item.longitude), title: item.city)
+                annotations.append(annotation)
+            }
+            mapAnnotations =  annotations
+        }
+        
+        if latitude != 0.0 && longitude != 0.0 {
+            let annotation = MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), title: "My Location")
+            mapAnnotations.append(annotation)
+        }
+    }
+    
+    func deleteFavouriteLocation(city: String) {
+        persistence.deleteFavouriteLocation(city: city)
+    }
+    
+    func updateCurrentLocation(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+        loadFavouriteLocations()
+    }
+    
+}
