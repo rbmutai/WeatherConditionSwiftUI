e16b56c4eee13f9047fd62e18e1c3ecdf29f34c2
Robert Mutai <rbmutai@gmail.com>
2025-01-22 12:57:45 +0300

    add unit tests

--- WeatherConditionSwiftUITests/WeatherConditionSwiftUITests.swift
+++ WeatherConditionSwiftUITests/WeatherConditionSwiftUITests.swift
@@ -10,27 +10,31 @@ import XCTest
 
 final class WeatherConditionSwiftUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testGetWeather() async {
+        let viewModel = WeatherViewModel(apiService: MockAPIService())
+       
+        await viewModel.getWeather(latitude: -1.3033, longitude: 36.8264)
+       
+        XCTAssertEqual(viewModel.city, "Nairobi South")
+        XCTAssertEqual(viewModel.conditions, "BROKEN CLOUDS")
+        XCTAssertEqual(viewModel.currentTemperature, "27.05º")
+        XCTAssertEqual(viewModel.maximumTemperature, "27.05º")
+        XCTAssertEqual(viewModel.minimumTemperature, "27.05º")
+        XCTAssertEqual(viewModel.weatherTheme, .cloud)
+        XCTAssertEqual(viewModel.forcastDetail[0].day, "Monday")
+        XCTAssertEqual(viewModel.forcastDetail[0].temperature, "27.05º")
+        XCTAssertEqual(viewModel.forcastDetail[0].theme, .cloud)
+       
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testGetLocationDetails() async {
+        let viewModel = WeatherViewModel(apiService: MockAPIService())
+       
+        await viewModel.getLocationDetail(latitude: -1.3033, longitude: 36.8264)
+        
+        XCTAssertEqual(viewModel.street, "35 Baricho Rd")
+        XCTAssertEqual(viewModel.province, "Nairobi County, Kenya")
+        
     }
 
 }
