e842a008c10de8075ed7064b264f22a57705238b
Robert Mutai <rbmutai@gmail.com>
2025-01-22 12:45:29 +0300

    add models

---
+++ WeatherConditionSwiftUI/LocationDetail.swift
@@ -0,0 +1,32 @@
+//
+//  LocationDetail.swift
+//  WeatherConditionSwiftUI
+//
+//  Created by Robert Mutai on 22/01/2025.
+//
+
+import Foundation
+
+struct LocationDetail: Decodable {
+    let results: [ResultDetail]
+}
+
+struct ResultDetail: Decodable {
+    let addressComponents: [AddressDetail]
+    let formattedAddress: String
+    let types: [String]
+    
+    enum CodingKeys: String, CodingKey {
+         case addressComponents = "address_components"
+         case formattedAddress = "formatted_address"
+         case types
+    }
+}
+
+struct AddressDetail: Decodable {
+    let longName: String
+    
+    enum CodingKeys:String, CodingKey {
+        case longName = "long_name"
+    }
+}
---
+++ WeatherConditionSwiftUI/Weather.swift
@@ -0,0 +1,44 @@
+//
+//  Weather.swift
+//  WeatherConditionSwiftUI
+//
+//  Created by Robert Mutai on 22/01/2025.
+//
+
+import Foundation
+
+struct Main: Decodable {
+       let temp: Double
+       let tempMin: Double
+       let tempMax: Double
+     
+       enum CodingKeys: String, CodingKey {
+            case temp
+            case tempMin = "temp_min"
+            case tempMax = "temp_max"
+       }
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Current: Decodable {
+    let weather: [Weather]
+    let main: Main
+    let name: String?
+    let dt: Int
+}
+
+struct Forcast: Decodable {
+    let list: [Current]
+}
+
+struct ForcastDetail {
+    let day: String
+    let theme: ConditionTheme
+    let temperature: String
+}
