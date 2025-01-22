dbb6273329e55365f2ec41e4dd4d166dcc002fbc
Robert Mutai <rbmutai@gmail.com>
2025-01-22 14:31:34 +0300

    provide default location

--- WeatherConditionSwiftUI/LocationServices.swift
+++ WeatherConditionSwiftUI/LocationServices.swift
@@ -10,7 +10,7 @@ import Combine
 import CoreLocation
 class LocationServices: NSObject, CLLocationManagerDelegate, ObservableObject {
     let locationManager = CLLocationManager()
-    @Published var coordinate: CLLocation?
+    @Published var coordinate: CLLocation = CLLocation(latitude: 0.0, longitude: 0.0)
     @Published var errorMessage = ""
     @Published var showErrorAlert = false
     override init() {
